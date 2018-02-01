5dda161c59c728802175cad43b7be2dd9ba6459c
Jacob Lindvald Pedersen <au520517@uni.au.dk>
2018-02-01 21:33:17 +0100

    Add project files.

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+    }
+
+    public interface IDoThings
+    {
+        void DoNothing();
+        int DoSomething(int number);
+        string DoSomethingElse(string input);
+
+    }
+
+    public class DoHickey:IDoThings
+    {
+       
+        public void DoNothing()
+        {
+            Console.WriteLine("Current Class"+this.GetType().Name+" and the methode is" +System.Reflection.MethodBase.GetCurrentMethod().Name);
+        }
+
+        public int DoSomething(int number)
+        {
+            Console.WriteLine("Current Class" + this.GetType().Name + " and the methode is" + System.Reflection.MethodBase.GetCurrentMethod().Name + number);
+            return number;
+        }
+
+        public string DoSomethingElse(string input)
+        {
+            Console.WriteLine("Current Class" + this.GetType().Name + " and the methode is" + System.Reflection.MethodBase.GetCurrentMethod().Name+input);
+            return input;
+        }
+    }
+
+    public class DoDickey:IDoThings
+    {
+        public void DoNothing()
+        {
+            Console.WriteLine("DoNothing");
+        }
+
+        public int DoSomething(int number)
+        {
+            Console.WriteLine("Number"+number);
+            return number;
+        }
+
+        public string DoSomethingElse(string input)
+        {
+            Console.WriteLine("Input"+input);
+            return input;
+        }
+    }
+}
---
+++ InterfaceExercise01/DoStuff.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary2;
+
+namespace InterfaceExercise01
+{
+    class DoStuff
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            Console.WriteLine("Want a DoHicky press 1 or DoDickey press 2 ????");
+            string inputfromuser = Console.ReadLine();
+            if (inputfromuser == "1")
+            {
+               IDoThings myStuff = new DoHickey();
+                myStuff.DoNothing();
+
+            }
+            else if(inputfromuser == "2")
+            {
+                IDoThings myStuff = new DoDickey();
+                myStuff.DoNothing();
+            }    
+            else
+            {
+                Console.WriteLine("You wrote something else then 1 or 2? Stupid.. ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
